b8b1c5ee91256c268b9e20034e54ebd72cf9a7f4
José Renato Royer <54846126+Joseroyer@users.noreply.github.com>
2023-08-26 15:57:55 -0300

    Ajustes

---
+++ DesafioES3/DesafioES3/Model/Cidades.cs
@@ -0,0 +1,23 @@
+﻿namespace DesafioES3.Model
+{
+    public class Cidades
+    {
+        private int Codigo_cidade { get; set; }
+        private string Nome { get; set; }
+
+        private Estados Estados { get; set; }
+
+        public Cidades()
+        {
+               Codigo_cidade = 0;
+               Nome = string.Empty;
+               Estados = new Estados();
+        }
+        public Cidades(int codigo_cidade, string nome, Estados estados)
+        {
+            Codigo_cidade = codigo_cidade;
+            Nome = nome;
+            Estados = ;
+        }
+    }
+}
---
+++ DesafioES3/DesafioES3/Model/Estados.cs
@@ -0,0 +1,19 @@
+﻿namespace DesafioES3.Model
+{
+    public class Estados
+    {
+        private int Codigo_Estado {get; set;}
+        private string Nome { get; set;}
+
+        public Estados() { 
+            Codigo_Estado = 0;
+            Nome = string.Empty;
+        }
+
+        public Estados(int codigo, string nome)
+        {
+            Codigo_Estado = codigo;
+            Nome = nome;
+        } 
+    }
+}
